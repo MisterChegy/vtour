a2ffc78d04173e05424a3af3a9c5de5989216bce
MisterChegy <3427639942@qq.com>
2019-09-10 21:10:50 +0800

    city页面动态数据渲染

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -3,9 +3,13 @@
     <city-header></city-header>
     <city-search></city-search>
     <city-list
-      
+      :cities="cities"
+      :hot="hotCities"
+      :letter="letter"
     ></city-list>
-    <city-alphabet></city-alphabet>
+    <city-alphabet
+      :cities="cities"
+    ></city-alphabet>
   </div>
 </template>
 
@@ -22,6 +26,33 @@ export default {
     CitySearch,
     CityList,
     CityAlphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: [],
+      letter: ''
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
